9e455887bb91632c82e4ce2258770b3e3d322b23
ericc346 <ericc346@nycstudents.net>
2024-09-20 12:23:08 -0400

    Finished?????

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         double tip = Double.parseDouble(ask.nextLine());
         System.out.print("Number of people: ");
         double ppl = Double.parseDouble(ask.nextLine());
-
+        System.out.println();
         double amount =  bill * (tip / 100);
         amount = Double.parseDouble(df.format(amount));
         double per = amount/ppl;
@@ -21,13 +21,12 @@ public class Main {
         double total = (bill/ppl + per);
         bill += amount;
 
-
-        System.out.println(df.format(amount));
-        System.out.println(df.format(bill));
-
-        System.out.println(df.format(per));
-        System.out.println(df.format(total));
-
+        System.out.println("___________________________________");
+        System.out.println("Total Tip Amount: $" + df.format(amount));
+        System.out.println("Total Bill Cost: $" + df.format(bill));
+        System.out.println("Tip per person: $" + df.format(per));
+        System.out.println("Total bill per person: $" + df.format(total));
+        System.out.println("___________________________________");
 
 
     }
