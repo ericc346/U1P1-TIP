5d918430b0449bcd803604f8738a35b72b9ce989
ericc346 <ericc346@nycstudents.net>
2024-09-20 11:06:00 -0400

    Work

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DecimalFormat df = new DecimalFormat("##.##");
+        Scanner ask = new Scanner(System.in);
+        System.out.print("Enter your Bill($): ");
+        double bill = Double.parseDouble(ask.nextLine());
+        System.out.print("Tip Percent: ");
+        double tip = Double.parseDouble(ask.nextLine());
+        System.out.print("Number of people: ");
+        double ppl = Double.parseDouble(ask.nextLine());
+
+
+        double amount = bill * (tip/100);
+        double per = amount/ppl;
+        double total = (bill/ppl + per);
+        bill += amount;
+
+
+        System.out.println(df.format(amount));
+        System.out.println(bill);
+
+        System.out.println(per);
+        System.out.println(total);
+
+
+
     }
 }
