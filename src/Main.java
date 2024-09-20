8c1ae17fd1000cf2c3ef22ad989cb31388512d67
ericc346 <ericc346@nycstudents.net>
2024-09-20 13:17:48 -0400

    finished

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
@@ -7,17 +6,24 @@ public class Main {
     public static void main(String[] args) {
         DecimalFormat df = new DecimalFormat("##.00");
         Scanner ask = new Scanner(System.in);
+
         System.out.print("Enter your Bill($): ");
         double bill = Double.parseDouble(ask.nextLine());
+
         System.out.print("Tip Percent: ");
         double tip = Double.parseDouble(ask.nextLine());
+
         System.out.print("Number of people: ");
         double ppl = Double.parseDouble(ask.nextLine());
+
         System.out.println();
+
         double amount =  bill * (tip / 100);
         amount = Double.parseDouble(df.format(amount));
+
         double per = amount/ppl;
         per = Double.parseDouble(df.format(per));
+
         double total = (bill/ppl + per);
         bill += amount;
 
@@ -27,7 +33,5 @@ public class Main {
         System.out.println("Tip per person: $" + df.format(per));
         System.out.println("Total bill per person: $" + df.format(total));
         System.out.println("___________________________________");
-
-
     }
 }
