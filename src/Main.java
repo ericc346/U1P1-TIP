73bfca00fe74644a8d8e30bac3ccaee0c72ab778
ericc346 <ericc346@nycstudents.net>
2024-09-23 13:07:52 -0400

    FINISHED WITH COMMENTS

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,34 @@
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
-
 public class Main {
     public static void main(String[] args) {
         DecimalFormat df = new DecimalFormat("##.00");
         Scanner ask = new Scanner(System.in);
 
-        System.out.print("Enter your Bill($): ");
-        double bill = Double.parseDouble(ask.nextLine());
+        System.out.print("Enter your Bill($): ");                   // ask for input of bill amount
+        double bill = Double.parseDouble(ask.nextLine());           //set inputted value as bill
 
-        System.out.print("Tip Percent: ");
-        double tip = Double.parseDouble(ask.nextLine());
+        System.out.print("Tip Percent: ");                          // ask for input of tip percent
+        double tip = Double.parseDouble(ask.nextLine());            //set inputted value as tip
 
-        System.out.print("Number of people: ");
-        double ppl = Double.parseDouble(ask.nextLine());
+        System.out.print("Number of people: ");                     // ask for input of people amount
+        double ppl = Double.parseDouble(ask.nextLine());            //set inputted value as ppl
 
         System.out.println();
 
-        double amount =  bill * (tip / 100);
-        amount = Double.parseDouble(df.format(amount));
+        double amount =  bill * (tip / 100);                        // (tip/100) = tip percent
+        amount = Double.parseDouble(df.format(amount));             // formats the variable amount as ##.00
 
-        double per = amount/ppl;
-        per = Double.parseDouble(df.format(per));
+        double per = amount/ppl;                                    // finds tip amount each person has to pay
+        per = Double.parseDouble(df.format(per));                   // formats the variable per as ##.00
 
-        double total = (bill/ppl + per);
-        bill += amount;
+        double total = (bill/ppl + per);                            // find total amount each person has to pay
+        bill += amount;                                             // adds the tip to the bill amount to have total bill cost
 
         System.out.println("___________________________________");
         System.out.println("Total Tip Amount: $" + df.format(amount));
-        System.out.println("Total Bill Cost: $" + df.format(bill));
+        System.out.println("Total bill Cost: $" + df.format(bill));
         System.out.println("Tip per person: $" + df.format(per));
         System.out.println("Total bill per person: $" + df.format(total));
         System.out.println("___________________________________");
